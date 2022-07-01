d60d639d3bc71743737dee6de5c2c892b9e792ed
BrigitteBCtde <104658943+BrigitteBCtde@users.noreply.github.com>
2022-06-30 19:34:35 -0500

    cambio mensajes
    cambio envio mensajes

--- visoft/SendSms.php
+++ visoft/SendSms.php
@@ -1,35 +1,45 @@
 <?php
-namespace Modules\CsConvivamos\Libs\ServiceResponse;
 
 /**
  * ServiceResponse class
  */
-class SendSms
-{
+
      /**
          * Build the message.
          *
          * @return $this
          */
 
-    public function build(string $messageSMS = "", $configSms, $numberTo)
+     function build(string $messageSMS = "", $configSms, $numberTo)
     {
-        $post['to'] = array($numberTo);
-        $post['text'] = $messageSMS;
-        $post['from'] = $configSms['to'];
-        $user = $configSms['user'];
-        $password = $configSms['pass'];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://sms.ubicarme.com/Api/rest/message");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
-        curl_setopt($ch, CURLOPT_HTTPHEADER,
-        array(
-        "Accept: application/json",
-        "Authorization: Basic ".base64_encode($user.":".$password)));
-        $result = curl_exec ($ch);
+        try {
+            $post['to'] = $numberTo;
+            $post['text'] = $messageSMS;
+            $post['from'] = $configSms['from'];
+            $user = $configSms['user'];
+            $password = $configSms['pass'];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, "http://sms.ubicarme.com/Api/rest/message");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+            curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array(
+            "Accept: application/json",
+            "Authorization: Basic ".base64_encode($user.":".$password)));
+            $result = curl_exec ($ch);
+            echo $result;
 
-        return $result;
+            return $result;
+        }catch (Exception $e){
+                    echo $e;
+                }
     }
-}
+        $configSms['user'] = 'Visof';
+        $configSms['pass'] = 'Maga300$';
+        $configSms['from'] = '573244220848';
+        $to = array('573244220848');
+
+        build('hola envio mensaje2', $configSms, $to);
+
+
